a5662cc70b6378ba6a4bbec740563fe248d6b5d3
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-04-24 23:02:25 +0200

    Wykresy funkcji i błędów

--- main.m
+++ main.m
@@ -1,4 +1,6 @@
 %% Zadanie przykladowe A
+clf;
+clc
 
 % Szukana funkcja: 1,1x^2+0,765x = 3,55x^3+0.74     x nalezy do <-1, 1>
 % Przeksztalcenie: 3,55x^3 - 1,1x^2 - 0,765x + 0,74 = 0
@@ -13,11 +15,34 @@ ddf = @(x) (21.3*x - 2.2);
 x = linspace(-1, 1, 2000);
 y = arrayfun(f, x);
 
+subplot(2, 1, 1);
 plot(x, y, "-", "DisplayName", "f(x) = 3.55*x^3 - 1.1*x^2 - 0.765*x + 0.74");
 grid on;
+hold on;
 xlabel("x");
 ylabel("f(x)");
 legend("Location", "best");
+
+[x0NR, bledyNR] = NewtonRaphson1(f, df, 0, 100, 10^(-8)); % Wynik jest zly bo x0 jest po zlejs stronie zbocza
+x0NR;
+
+[x0NR, bledyNR] = NewtonRaphson1(f, df, -1, 100, 10^(-8)); %Teraz jest dobrze
+x0NR
+
+[x0Bi, blledyBi] = bisekcja(f, -1, 1, 10000, 10^(-8));
+
+plot(x0NR, f(x0NR), "o", "DisplayName", "Wynik metoda Newtona Raphsona");
+plot(x0Bi, f(x0Bi), "o", "DisplayName", "Wynik metoda bisekcji");
+
+%Wykres bledow
+subplot(2, 1, 2);
+hold on;
+grid on;
+semilogy(bledyNR, "DisplayName", "B??dy metody Newtona Raphsona");
+semilogy(blledyBi, "DisplayName", "B??dy metody bisekcji");
+
+legend("Location", "best");
+
 
 %% Testy NewtonRaphson1
 clc;
@@ -25,7 +50,7 @@ clc;
 f   = @(x) (4*x^3 + 1*x^2 + 2*x + 8);
 df  = @(x) (12*x^2 + 2*x + 2);
 
-[x0, b] = NewtonRaphson1(f, df, 1, 2, 10^(-8))
+[x0, b] = NewtonRaphson1(f, df, 1, 100, 10^(-8))
 
 %% Testy bisekcji
 clc;
